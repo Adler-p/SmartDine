a40762fb784f8861af35fd66fce896796d745529
Zufiqqar <57473144+Zufiqqar@users.noreply.github.com>
2025-04-24 13:33:04 +0800

    Update cart-payment.js

--- pages/menu/cart-payment.js
+++ pages/menu/cart-payment.js
@@ -1,3 +1,5 @@
+'use client'; // This is mandatory
+
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import styles from './CartPaymentPage.module.css';
